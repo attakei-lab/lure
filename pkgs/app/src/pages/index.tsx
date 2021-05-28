f7872dc08e10869c8ce41d548cede31119e9d301
Kazuya Takei <myself@attakei.net>
2021-05-28 23:45:21 +0900

    Init: define next.js app

---
+++ pkgs/app/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import 'react';
+
+export const Page = () => (
+  <>
+    <header>
+      <h1>Lure</h1>
+    </header>
+    <main>
+      <p>
+        Lureは、小〜中規模組織向けのシンプルなドキュメント共有アプリケーションキットです。
+      </p>
+    </main>
+    <footer>
+      <p>Powered by @attakei</p>
+    </footer>
+  </>
+);
+
+export default Page;
