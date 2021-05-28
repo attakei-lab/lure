27c86de3e6778546e3c56733a3d81ef52280de95
Kazuya Takei <myself@attakei.net>
2021-05-29 00:44:30 +0900

    app: use firebase-ui for authentication

--- pkgs/app/src/pages/index.tsx
+++ pkgs/app/src/pages/index.tsx
@@ -1,19 +1,44 @@
-import 'react';
+import { useEffect, useState } from 'react';
+import firebase from 'firebase';
+import { useFirebaseAuth } from 'firebase-react-provider';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-export const Page = () => (
-  <>
-    <header>
-      <h1>Lure</h1>
-    </header>
-    <main>
-      <p>
-        Lureは、小〜中規模組織向けのシンプルなドキュメント共有アプリケーションキットです。
-      </p>
-    </main>
-    <footer>
-      <p>Powered by @attakei</p>
-    </footer>
-  </>
-);
+import { firebaseUIConfig } from '../config';
 
+export const Page = () => {
+  const { appAuth, authConstructor } = useFirebaseAuth();
+  const [inProcess, setInProcess] = useState(true);
+
+  useEffect(() => {
+    const onWaiting = !(appAuth && authConstructor);
+    if (onWaiting) return;
+    setInProcess(false);
+  }, [appAuth, authConstructor]);
+
+  return (
+    <>
+      <header>
+        <h1>Lure</h1>
+      </header>
+      {inProcess ? (
+        <main>
+          <p>ロード中</p>
+        </main>
+      ) : (
+        <main>
+          <p>
+            Lureは、小〜中規模組織向けのシンプルなドキュメント共有アプリケーションキットです。
+          </p>
+          <StyledFirebaseAuth
+            uiConfig={firebaseUIConfig}
+            firebaseAuth={appAuth}
+          />
+        </main>
+      )}
+      <footer>
+        <p>Powered by @attakei</p>
+      </footer>
+    </>
+  );
+};
 export default Page;
