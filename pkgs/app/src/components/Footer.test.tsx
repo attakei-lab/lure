cadb267e349d5b2f595ba033a0db326ec2637d26
Kazuya Takei <myself@attakei.net>
2021-06-02 02:18:48 +0900

    app: add first test(view container)

---
+++ pkgs/app/src/components/Footer.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, RenderResult, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Footer from './Footer';
+
+describe('Footer', () => {
+  let renderResult: RenderResult;
+  beforeEach(() => {
+    // 準備処理 テスト対象Componentの描画
+    renderResult = render(<Footer />);
+  });
+  afterEach(() => {
+    // テスト終了後処理 テスト対象のアンマウント
+    renderResult.unmount();
+  });
+  describe('初期状態', () => {
+    test('クレジット情報が表示されている', async () => {
+      await waitFor(() => {
+        expect(renderResult.getByRole('contentinfo')).toHaveTextContent(
+          /Powered by/
+        );
+      });
+    });
+  });
+});
