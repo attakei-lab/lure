743f05e2a6c646b2871aa7521539acb8d3d3cfdf
Kazuya Takei <myself@attakei.net>
2021-06-04 15:59:22 +0900

    app: add example of configuration module

---
+++ pkgs/app/src/config.example.js
@@ -0,0 +1,14 @@
+// FOR YOU: Paste your application config of firebase
+export const firebaseConfig = {};
+
+export const firebaseUIConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    // FOR YOU: Setting up your authentication providers
+  ],
+};
+
+export const appConfig = {
+  publicPages: [],
+};
