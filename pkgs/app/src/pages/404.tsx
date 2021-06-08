c024532b846db05f0f221c1b8c2f82c352c83c1e
Kazuya Takei <myself@attakei.net>
2021-06-08 15:17:54 +0900

    app: refactor content

--- pkgs/app/src/pages/404.tsx
+++ pkgs/app/src/pages/404.tsx
@@ -1,21 +1,21 @@
 import Head from 'next/head';
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
-import BaseLayout from '../components/templates/BaseLayout';
+import Template from '../components/templates/SimpleContent';
 
-export const Page = () => {
-  return (
-    <BaseLayout>
-      <Head>
-        <title>Not Found</title>
-      </Head>
-      <main>
-        <Container>
-          <Header as="h1">Not Found</Header>
-          <p>リクエストされたURLは存在しません</p>
-        </Container>
-      </main>
-    </BaseLayout>
-  );
+const content = {
+  title: 'Not Found',
+  body: `
+リクエストされたURLは存在しません
+`,
 };
+
+export const Page = () => (
+  <>
+    <Head>
+      <title>{content.title} | Lure</title>
+    </Head>
+    <Template content={content} />
+  </>
+);
+
 export default Page;
