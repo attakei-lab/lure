2519640734ba147ec2641e5e0b7e5cb5528401c8
Kazuya Takei <myself@attakei.net>
2021-06-19 14:03:00 +0900

    frontend: add stories

---
+++ pkgs/frontend/src/components/organisms/Footer/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { View } from '.';
+
+export const Default = () => <View />;
+
+export default {
+  title: 'organisms/Footer',
+  component: View,
+} as Meta;
---
+++ pkgs/frontend/src/components/organisms/Header/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { View } from '.';
+
+export const Default = () => <View />;
+
+export default {
+  title: 'organisms/Header',
+  component: View,
+} as Meta;
