03badff8faa2b11f1619bfe7af0dc7c0e4f63f33
Kazuya Takei <myself@attakei.net>
2021-06-27 01:53:06 +0900

    frontned: configure footer

--- packages/frontend/src/components/organisms/Footer/index.tsx
+++ packages/frontend/src/components/organisms/Footer/index.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import { Container, Segment } from 'semantic-ui-react';
 
+export const Styles: { [key: string]: React.CSSProperties } = {
+  Segment: {
+    paddingTop: '3rem',
+    paddingBottom: '2rem',
+    borderRadius: 0,
+  },
+  Link: {
+    color: 'white',
+    textDecorationLine: 'underline',
+  },
+};
+
 /**
  * ページ全体のフッターを表示させる
  */
-export const View = () => (
+export const View: React.FC = () => (
   <footer>
-    {/* TODO: use other styling */}
-    <Segment
-      inverted
-      vertical
-      color="grey"
-      style={{ paddingTop: '3rem', paddingBottom: '2rem', borderRadius: 0 }}
-    >
-      <Container>
-        <p>Powered by @attakei</p>
+    <Segment inverted vertical color="grey" style={Styles.Segment}>
+      <Container textAlign="right">
+        <p>
+          {'Powered by '}
+          <a
+            href="https://github.com/attakei-lab/lure"
+            target="_blank"
+            rel="noreferrer"
+            style={Styles.Link}
+          >
+            @attakei
+          </a>
+        </p>
       </Container>
     </Segment>
   </footer>
