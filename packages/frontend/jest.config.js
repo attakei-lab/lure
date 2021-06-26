f3807a31991dc18dba637cd27c2c095fcbc3224e
Kazuya Takei <myself@attakei.net>
2021-06-27 01:07:28 +0900

    frontend: configuration for ts-jest

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
+    '@/(.+)': '<rootDir>/src/$1',
   },
   globals: {
     'ts-jest': {
