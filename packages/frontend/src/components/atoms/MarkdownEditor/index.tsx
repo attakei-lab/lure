b31311d6011ac06a141ef3731a37e669f2c04148
Kazuya Takei <myself@attakei.net>
2021-06-26 23:29:45 +0900

    frontend: revert editor to work

--- packages/frontend/src/components/atoms/MarkdownEditor/index.tsx
+++ packages/frontend/src/components/atoms/MarkdownEditor/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import { Editor } from '@bytemd/react';
+import dynamic from 'next/dynamic';
+import React, { useMemo } from 'react';
+import { Options } from 'easymde';
+
+// SimpleMDEはサーバーサイドで動作しないため、ダイナミックインポートしている
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
 
 export type Props = {
   /** フォーム内のソースが編集された際の処理 */
@@ -14,8 +20,15 @@ export type Props = {
  * @param Props props
  * @todo コードブロックのサポート（要Prism.js）
  */
-export const View: React.FC<Props> = ({ handleInput, input }) => (
-  <Editor onChange={handleInput} value={input} />
-);
+export const View: React.FC<Props> = ({ handleInput, input }) => {
+  const options = useMemo<Options>(() => {
+    return {
+      sideBySideFullscreen: false,
+      spellChecker: false,
+    };
+  }, [window]);
+
+  return <SimpleMDE onChange={handleInput} options={options} value={input} />;
+};
 
 export default View;
