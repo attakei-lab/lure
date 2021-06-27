f443420af48b95c4da6e498a28fde5dea6b59c0e
Kazuya Takei <myself@attakei.net>
2021-06-27 15:59:10 +0900

    frontend: re-remove smd

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -7,7 +7,6 @@ import LoginTemplate from '@/components/templates/Login';
 import { AppConfigContext, AppConfigProvider } from '@/contexts/config';
 import { FirebaseAppContext, FirebaseAppProvider } from '@/contexts/firebase';
 import 'bytemd/dist/index.min.css';
-import 'easymde/dist/easymde.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import '@/style.css';
 
