8883e8938b18ffd45984179317447e4e7b4fa825
Kazuya Takei <myself@attakei.net>
2021-06-28 17:02:59 +0900

    frontend: render posts in top-page

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import Login from '@/components/organisms/Login';
-import BaseLayout from '@/components/templates/BaseLayout';
-import { Container } from 'semantic-ui-react';
+import React, { useContext } from 'react';
+import { usePosts } from '@/applications/posts/hooks';
+import { Wrapper as ErrorWrapper } from '@/components/templates/Error';
+import { Wrapper as LoadingWrapper } from '@/components/templates/Loading';
+import ViewTemplate from '@/components/templates/PostList';
+import { FirebaseAppContext } from '@/contexts/firebase';
 
+/**
+ * トップページ
+ *
+ * 最新記事を表示する
+ */
 export const Page = () => {
+  const { app } = useContext(FirebaseAppContext);
+  const { posts, error, loading } = usePosts(app);
+
   return (
-    <BaseLayout>
-      <main>
-        <Container>
-          <p>
-            Lureは、小〜中規模組織向けのシンプルなドキュメント共有アプリケーションキットです。
-          </p>
-          <Login />
-        </Container>
-      </main>
-    </BaseLayout>
+    <LoadingWrapper loading={loading}>
+      <ErrorWrapper error={error}>
+        {posts && <ViewTemplate headingText="" posts={posts} />}
+      </ErrorWrapper>
+    </LoadingWrapper>
   );
 };
 export default Page;
