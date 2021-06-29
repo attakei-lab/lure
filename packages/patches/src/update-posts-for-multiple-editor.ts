257af75c98a26fec44f1b5b0c8e0bd59df0c18f4
Kazuya Takei <myself@attakei.net>
2021-06-29 20:39:38 +0900

    patch: set document metadata to all posts

---
+++ packages/patches/src/update-posts-for-multiple-editor.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+import * as admin from 'firebase-admin';
+
+/**
+ * Postデータの更新
+ *
+ * 更新者と作成者の情報を表示等で使うため、データ構造の更新を行う
+ */
+export const main = async () => {
+  await admin
+    .firestore()
+    .collection('posts')
+    .get()
+    .then((querySnapshot) => {
+      return querySnapshot.docs.map((snapshot) => {
+        const data = snapshot.data();
+        data.createdAt = data.created;
+        data.createdRef = data.authorRef;
+        data.updatedAt = data.updated;
+        data.updatedRef = data.authorRef;
+        delete data.createdBy;
+        delete data.updatedBy;
+        data.authors = [data.authorRef];
+        return snapshot.ref.set(data);
+      });
+    })
+    .then((procs) => Promise.all(procs));
+};
+
+(async () => {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(
+    path.join(process.cwd(), 'firebase-adminsdk.json')
+  );
+  admin.initializeApp();
+  await main();
+})();
