269e8cb69783279b8884b4b306f274830b265ef0
Kazuya Takei <kazuya.takei@nijibox.co.jp>
2021-07-21 15:50:12 +0900

    Configure for posts page

--- packages/frontend/src/pages/posts.tsx
+++ packages/frontend/src/pages/posts.tsx
@@ -1,21 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { usePostsWithCursor } from '@/applications/posts/hooks';
 import { Wrapper as ErrorWrapper } from '@/components/templates/Error';
 import { Wrapper as LoadingWrapper } from '@/components/templates/Loading';
 import ViewTemplate from '@/components/templates/PostList';
 import { FirebaseAppContext } from '@/contexts/firebase';
-import { usePosts } from '@/applications/posts/hooks';
+import { useEffect } from 'react';
 
 export const Page = () => {
   const { app } = useContext(FirebaseAppContext);
-  const { loading, error, posts } = usePosts(app);
+  const { posts, error, loading, fetchNext, hasNext } = usePostsWithCursor(app);
+  const [nextDisabled, setNextDisabled] = useState<boolean>(false);
+  const [nextLoading, setNextLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (nextLoading) {
+      setNextDisabled(true);
+    }
+    setNextDisabled(!hasNext);
+  }, [nextLoading, hasNext]);
+
+  const handleFetchNext = async () => {
+    setNextDisabled(true);
+    setNextLoading(true);
+    try {
+      await fetchNext();
+    } catch (e) {
+      setNextLoading(false);
+      setNextDisabled(!hasNext);
+      throw e;
+    }
+    setNextLoading(false);
+    setNextDisabled(!hasNext);
+  };
 
   return (
     <LoadingWrapper loading={loading}>
       <ErrorWrapper error={error}>
-        {posts && <ViewTemplate headingText="記事一覧" posts={posts} />}
+        {posts && (
+          <ViewTemplate
+            headingText="記事一覧"
+            posts={posts}
+            nextButtonDisabled={nextDisabled}
+            nextButtonHandler={handleFetchNext}
+            nextButtonLoading={nextLoading}
+          />
+        )}
       </ErrorWrapper>
     </LoadingWrapper>
   );
 };
-
 export default Page;
